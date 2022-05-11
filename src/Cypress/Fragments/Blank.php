a31e6f7ae376b7f80429990d2147014636ac50b7
Ismail Zare <zarehesmaiel@gmail.com>
2022-05-11 20:36:02 +0430

    removed wrong title method

--- src/Cypress/Fragments/Blank.php
+++ src/Cypress/Fragments/Blank.php
@@ -5,9 +5,5 @@ namespace Zareismail\Mason\Cypress\Fragments;
 use Zareismail\Cypress\Fragment;   
 
 class Blank extends Fragment
-{       
-	public function title($request)
-	{
-		return static::fragment()->getUrl('test');
-	}
+{        
 }
